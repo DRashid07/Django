154531ccfeaa6cdd182d84cdfcaa534c5e980798
DRashid07 <rashided@.code.edu.az>
2025-10-27 10:15:30 +0400

    Admin və modellər yeniləndi

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Salam, Django Layihən işlədi! 😊</h1><p>Polls səhifəsinə keçmək üçün <a href='/polls/'>buraya kliklə</a>.</p>")
 
 urlpatterns = [
+    path('', home),  # Ana səhifə
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),  # BU SƏTR ÇOX VACİBDİR!
 ]
-
 
--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    search_fields = ["question_text"]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+
